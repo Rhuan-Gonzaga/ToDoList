38663e8d1f54aef7990ce052c3affa7a77cab01a
Rhuan-Gonzaga <rhuangonzaga22@gmail.com>
2021-12-16 12:31:53 -0300

    Adicionando itens na lista

--- src/Todo.js
+++ src/Todo.js
@@ -3,23 +3,38 @@ import "./Todo.css";
 
 function Todo(){
 
+    //Valores do input
     const [texto,setTexto] = useState("")
+    //Tarefas Adicionadas
+    const [tarefasAtual, setTarefasAtual] = useState([]);
 
-    function addItems(event){
-        let txt = event.target.value;
-        setTexto(txt)
+    //Função cria uma tarefa
+    function tarefas(event){
+        let  valorTarefa = event.target.value;
+        setTexto(valorTarefa)
+    }
+
+    //Frunção cria uma nova tarefa, sem excluir as anteriores
+    function novaTarefa(event){
+        event.preventDefault();
+        if(texto){
+            setTarefasAtual([...tarefasAtual,texto])
+            setTexto("");
+        }
+       
     }
 
     return(
         <div className="container">
             <h1>Teste</h1>
             <form>
-                <input onChange={addItems}type="text"></input>
-                <button>ADD</button>
+                <input onChange={tarefas}type="text" value={texto}></input>
+                <button onClick={novaTarefa}>ADD</button>
             </form>
 
             <ul>
-                <li>{texto}</li>
+                {/*Criando uma lista de tarefas*/}
+                {tarefasAtual.map(tarefa => <li>{tarefa}</li>)}
             </ul>
         </div>
        
